6e40c1508087497fe6f6365aec3972bf72ab61b4
Andrew Henry <andymitchhank@users.noreply.github.com>
2017-09-05 14:57:45 -0500

    Update package setup using hatch

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Bessie - Base API Client
 
+-----
+
+**Table of Contents**
+
+* [License](#license)
+
+## License
+
+bessie is distributed under the terms of the
+[MIT License](https://choosealicense.com/licenses/mit).
--- setup.py
+++ setup.py
@@ -1,123 +1,49 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
 
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pip install twine
+with open('bessie/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
 
-import io
-import os
-import sys
-from shutil import rmtree
+with open('README.md', 'rb') as f:
+    readme = f.read().decode('utf-8')
 
-from setuptools import find_packages, setup, Command
+REQUIRES = []
 
-# Package meta-data.
-NAME = 'bessie'
-DESCRIPTION = 'Bessie - Base API Client'
-URL = 'https://github.com/andymitchhank/bessie'
-EMAIL = 'andymitchhank@users.noreply.github.com'
-AUTHOR = 'Andrew Henry'
-
-# What packages are required for this module to be executed?
-REQUIRED = [
-    'requests'
-]
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
-with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-with open(os.path.join(here, NAME, '__version__.py')) as f:
-    exec(f.read(), about)
-
-
-class PublishCommand(Command):
-    """Support setup.py publish."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-    
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except FileNotFoundError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPi via Twine…')
-        os.system('twine upload dist/*')
-
-        sys.exit()
-
-
-# Where the magic happens:
 setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    author=AUTHOR,
-    author_email=EMAIL,
-    url=URL,
-    packages=find_packages(exclude=('tests',)),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
-    install_requires=REQUIRED,
-    include_package_data=True,
+    name='bessie',
+    version=version,
+    description='Bessie - Base API Client',
+    long_description=readme,
+    author='Andrew Henry',
+    author_email='andymitchhank@users.noreply.github.com',
+    maintainer='Andrew Henry',
+    maintainer_email='andymitchhank@users.noreply.github.com',
+    url='https://github.com/andymitchhank/bessie',
     license='MIT',
+
+    keywords=[
+        '',
+    ],
+
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Base API Client',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
-    # $ setup.py publish support. 
-    cmdclass={
-        'publish': PublishCommand,
-    },
+
+    install_requires=REQUIRES,
+    tests_require=['coverage', 'pytest'],
+
+    packages=find_packages(),
 )
