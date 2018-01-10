c0cd5ccb63779a70db3b376157b6f39db6062dee
Andrew Henry <andymitchhank@users.noreply.github.com>
2018-01-10 10:18:24 -0600

    Update numbers api example

--- examples/Numbers/numbers.py
+++ examples/Numbers/numbers.py
@@ -1,22 +1,24 @@
 from bessie import BaseClient, Endpoint
 
 
+types = ['trivia', 'math', 'date', 'year']
+numbers = ['<number>', 'random']
+available_endpoints = [Endpoint('GET', f'{n}/{t}', None) for t in types for n in numbers] + [Endpoint('GET', f'{n}', None) for n in numbers]
+available_endpoints.append(Endpoint('GET', '<month>/<day>/date', None))
+
+print(available_endpoints)
+
 class NumbersApi(BaseClient):
 
-	endpoints = [
-		Endpoint('GET', 'random', None),
-		Endpoint('GET', '<n>/trivia', None),
-		Endpoint('GET', '<n>/date', None),
-		Endpoint('GET', '<n1>/<n2>/date')
-	]
+	endpoints = available_endpoints
 	base_url='http://numbersapi.com'
 
 
 if __name__ == '__main__':
 	api = NumbersApi()
-	print(api.n(4).trivia.get().text)
-	print(api.n(4).date.get().text)
-	print(api.n1(4).n2(3).date.get().text)
+	print(api.number(4).trivia.get().text)
+	print(api.number(4).date.get().text)
+	print(api.month(4).day(3).date.get().text)
 	print(api.random.get(min=10, max=20).text)
 
 
